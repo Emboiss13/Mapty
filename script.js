306c5fc374630e2796edd2cbdb95c4373b8bf06e
Emboiss13 <116002662+Emboiss13@users.noreply.github.com>
2025-09-27 21:01:11 +0100

    Adding my browser coordinates

--- script.js
+++ script.js
@@ -20,14 +20,16 @@ if (navigator.geolocation) {
         function (position) {
             const { latitude } = position.coords;
             const { longitude } = position.coords;
+
+            const coords = [latitude, longitude];
             
-            var map = L.map('map').setView([51.505, -0.09], 13);
+            var map = L.map('map').setView(coords, 13);
 
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
 
-            L.marker([51.5, -0.09]).addTo(map)
+            L.marker(coords).addTo(map)
                 .bindPopup('A pretty CSS popup.<br> Easily customizable.')
                 .openPopup();
         }
