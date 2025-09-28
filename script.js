42618525f763c244f1a4b98a39735db70aad1914
Emboiss13 <116002662+Emboiss13@users.noreply.github.com>
2025-09-28 15:57:54 +0100

    Enhance map marker popup with custom styling and options

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 'use strict';
 
 // prettier-ignore
+//GLOBAL VARIABLES  
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let map, mapEvent;
+
+//-------------------------------
+//DOM Elements
+//-------------------------------
 
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
@@ -23,7 +30,7 @@ if (navigator.geolocation) {
 
             const coords = [latitude, longitude];
             
-            var map = L.map('map').setView(coords, 13);
+            map = L.map('map').setView(coords, 13);
 
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -33,15 +40,12 @@ if (navigator.geolocation) {
             //Displaying a Map Marker
             //-------------------------------
             //We are adding an event listener to the map object
-            map.on('click', function (mapEvent) { 
-                const { lat, lng } = mapEvent.latlng;
-
-                L.marker([lat, lng]).addTo(map)
-                .bindPopup('Workout')
-                .openPopup();
+            map.on('click', function (mapE) { 
+                mapEvent = mapE;
+                //Display form
+                form.classList.remove('hidden');
+                inputDistance.focus();
             })
-                
-
 
         }
     ),
@@ -50,8 +54,35 @@ if (navigator.geolocation) {
     }
 }
 
+form.addEventListener('submit', function (e) {
 
+    //Prevent form from submitting
+    e.preventDefault();
 
+    //Clear input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+    // Display marker
+    const { lat, lng } = mapEvent.latlng;
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+        })
+    )
+    .setPopupContent('Workout!')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+    //We are using closest to go up in the DOM tree to find the parent element with the class form__row
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
 
 
     
